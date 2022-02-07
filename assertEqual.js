868f3c40f34178f4fb5972b788fa1278c6884f23
Ryan <ryan.maceachern@gmail.com>
2022-02-07 15:42:32 -0800

    Created a simple assertion function for use in testing future code

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Bingo", "Bingo");
