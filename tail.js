8356377b8d594b4238221f5a0afcecac9edb96c7
Ryan <ryan.maceachern@gmail.com>
2022-02-07 16:31:48 -0800

    Created a simple function for retrieving the tail (every element, but the first) of an array

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (array) => {
+  return (array.slice(1));
+};
