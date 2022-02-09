507ba679bdf5836df7e084c6ef3481bffcfd0782
Ryan <ryan.maceachern@gmail.com>
2022-02-09 13:50:08 -0800

    Completed middle function for finding the middle element of an array

--- middle.js
+++ middle.js
@@ -10,3 +10,19 @@ const assertArraysEqual = (arr1, arr2) => {
   eqArrays(arr1, arr2) ? console.log("🟢🟢🟢 Assertion Passed") : console.log("🛑🛑🛑 Assertion Failed");
 };
 
+const middle = (array) => {
+  let midNum = [];
+  const midOfArray = array.length / 2;
+  // if array has one or two elements, there is no middle, so return an empty array.
+  if (array.length <= 2) {
+    return midNum;
+  }
+  // ternary operator determines what elements are pushed to midNum based on whether the array length is odd or even. Odd pushes 1 element, even pushes the 2 middle elements.
+  array.length % 2 !== 0 ? midNum.push(array[Math.floor(midOfArray)]) : midNum.push(array[midOfArray - 1]) && midNum.push(array[midOfArray]);
+  return midNum;
+};
+
+assertArraysEqual(middle([1, 2, 5, 18, 4, 10]), [5, 18]); // Pass!
+assertArraysEqual(middle([1, 2]), []); // Pass!
+assertArraysEqual(middle([1, 2, 3]), [2]); // Pass!
+
