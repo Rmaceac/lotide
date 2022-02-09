61aee5521c60851497aabf708d571ae0e9284de2
Ryan <ryan.maceachern@gmail.com>
2022-02-08 22:44:29 -0800

    Tiny revisions to two files and eqArrays.js completed for Lotide project

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,8 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Bingo", "Bingo");
+// Test code
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 2);
+// assertEqual("Bingo", "Bingo");
---
+++ reverse.js
@@ -0,0 +1,7 @@
+const array = process.argv.slice(2)
+for (let i = 0; i < array.length; i++) {
+  let result = ""
+  for (let x = array[i].length - 1; x >= 0; x--) {
+    result += array[i][x];
+  } console.log(result);
+};
