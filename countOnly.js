0332c27baa829a00293261cce1130f9a4156ce7a
Ryan <ryan.maceachern@gmail.com>
2022-02-09 15:28:35 -0800

    built a tricky function for counting wanted items from an array and storing counts in an object

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  const countObj = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (countObj[item]) {
+        countObj[item] += 1;
+       } else {
+        countObj[item] = 1;
+     }
+    }
+  } return countObj;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log(result1);
+
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
