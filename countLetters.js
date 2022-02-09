3b07feb4253969183b0ceded1fc34a818916adf3
Ryan <ryan.maceachern@gmail.com>
2022-02-09 15:47:41 -0800

    created new function to count letter occurances in a string

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (string) => {
+  const letterCount = {};
+  for (const letter of string) {
+    if (letter !== " ") {  
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  } return (letterCount);
+}
+
+// declare empty object to return at end of function
+// loop through string argument with a for...of loop
+// find the index of the letters of the string (in the loop (string[i])
+// add letters via index to object as a key
+// letter++ every time there is an additional occurance
+// try wrapping "letter" in quotation marks to see what happens
+
+
+countLetters("ryan maceachern you are number one")
--- countOnly.js
+++ countOnly.js
@@ -20,21 +20,21 @@ const countOnly = (allItems, itemsToCount) => {
 };
 
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-console.log(result1);
+// console.log(result1);
 
 // assertEqual(result1["Jason"], 1);
 // assertEqual(result1["Karima"], undefined);
