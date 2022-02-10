e53e780b09c60345e748c315efb9639c3a6e3bdf
Ryan <ryan.maceachern@gmail.com>
2022-02-09 19:43:30 -0800

    new function for searching objects by value

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, value) => {
+  for (const [k, v] of Object.entries(obj)) {
+    if (v === value) {
+      return k;
+    }
+  } 
+  return undefined;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// TEST CODE:
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
