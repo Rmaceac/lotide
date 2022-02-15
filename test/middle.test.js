c4ffef2d88d80f9b549220667a11888ffd86da8f
Ryan <ryan.maceachern@gmail.com>
2022-02-14 17:09:10 -0800

    refactored middle.test.js to use mocha syntax

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,6 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 5, 18, 4, 10]), [5, 18]); // Pass!
-assertArraysEqual(middle([1, 2]), []); // Pass!
-assertArraysEqual(middle([1, 2, 3]), [2]); // Pass!
+describe("should return the element in the middle (1 or 2) of a given array", () => {
+  it('should return [3] when given [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should return an empty array when given an array with 2 or fewer elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("should return the two middle element if given an array with an equal number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
